ae2be35a6c587ba27e461a2803e09bcd4162d86a
tutte2k <45877330+tutte2k@users.noreply.github.com>
2022-03-11 16:52:55 +0100

    Hej (#3)

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ObserverPattern
 {
@@ -7,43 +8,68 @@ namespace ObserverPattern
     {
         static void Main(string[] args)
         {
-            Channel channel = new Channel("Lowder With Crowder");
-            var sub1 = new Subscriber("Jordan Peterson");
-            var sub2 = new Subscriber("Sam Harris");
+            Channel channel1 = new Channel("Lowder With Crowder");
+            Channel channel2 = new Channel("Kalle Med Jointen");
+            Channel channel3 = new Channel("How Its Made");
+            Subscriber sub1 = new Subscriber("Jordan Peterson");
+            Subscriber sub2 = new Subscriber("Sam Harris");
+            Subscriber sub3 = new Subscriber("Raffe");
 
-            sub1.Subscribe(channel);
-            sub2.Subscribe(channel);
+            sub3.Subscribe(channel1);
+            sub3.Subscribe(channel2);
+            sub3.Subscribe(channel3);
 
-            channel.Upload("God Is Dead!");
+            sub1.Subscribe(channel1);
+            sub1.Subscribe(channel2);
 
-            sub2.UnSubscribe();
+            sub2.Subscribe(channel1);
+            sub2.Subscribe(channel2);
 
-            channel.Upload("The God Delusion");
+
+            channel1.Upload("God Is Dead!");
+            channel2.Upload("Making roaches from keef only");
+
+            sub2.UnSubscribe(channel1);
+            sub2.UnSubscribe(channel2);
+            channel1.Upload("The God Delusion");
+            channel2.Upload("Cross joint engineering");
+
+            sub1.GetSubscriptions();
+            sub2.GetSubscriptions();
+            sub3.GetSubscriptions();
+            sub3.UnSubscribe(channel1);
+            sub3.UnSubscribe(channel2);
+            channel3.Upload("Hot Dogs");
+            channel3.Upload("Babies");
+            channel3.Upload("Water");
+            sub3.GetSubscriptions();
         }
     }
 
     public class Channel
     {
         private List<Subscriber> Subscribers { get; set; }
-        public string channelName { get; private set; }
-        
+        private List<Video> Videos { get; set; }
+        public string channelName { get; set; }
+
         public Channel(string channelName)
         {
             this.channelName = channelName;
             Subscribers = new List<Subscriber>();
+            Videos = new List<Video>();
         }
 
-        private void NotifySubscribers(string videoName)
+        private void NotifySubscribers(string videoName, string url)
         {
-            foreach (var subscriber in Subscribers)
+            foreach (Subscriber subscriber in Subscribers)
             {
-                subscriber.Update(videoName);
+                subscriber.Update(videoName, channelName, url);
             }
         }
 
         public void AddSubscriber(Subscriber subscriber)
         {
-            
+
             Subscribers.Add(subscriber);
             Console.WriteLine($"[NOTIFICATION IN CHANNEL CLASS] => {subscriber.Name} has subscribed to {channelName}");
         }
@@ -57,35 +83,77 @@ namespace ObserverPattern
 
         public void Upload(string videoName)
         {
-            NotifySubscribers(videoName);
+            Video video = new Video(this, videoName);
+            Videos.Add(video);
+            NotifySubscribers(videoName, video.Url);
+        }
+        public int VideosCount()
+        {
+            return Videos.Count;
+        }
+    }
+
+    public class Video
+    {
+        private Channel Channel;
+        public string Url;
+        private string VideoName;
+
+        public Video(Channel channel, string videoName)
+        {
+            Channel = channel;
+            Url = SetUrl(videoName);
+            VideoName = videoName;
+
+        }
+        private string SetUrl(string videoName)
+        {
+            return $"https://video.com/{Channel.channelName.Replace(" ", "")}/{Channel.VideosCount()}/";
         }
     }
 
     public class Subscriber
     {
         public string Name { get; private set; }
-        public Channel Channel { get; set; }
+
+        private List<Channel> Subscriptions { get; set; }
 
         public Subscriber(string Name)
         {
             this.Name = Name;
+            Subscriptions = new List<Channel>();
         }
+        public void GetSubscriptions()
+        {
+            if (Subscriptions.Count>0)
+            {
+                string str = $"{Name} is subscribed to channels:\n";
+                for (int i = 0; i < Subscriptions.Count; i++)
+                {
+                    str += $"{Subscriptions[i].channelName}\n";
+                }
+                Console.WriteLine(str);
+            }
+            
+        }
+
+
+
         public void Subscribe(Channel channel)
         {
-            Channel = channel;
-            Channel.AddSubscriber(this);
+            channel.AddSubscriber(this);
+            Subscriptions.Add(channel);
 
         }
-
-        public void UnSubscribe()
+        public void UnSubscribe(Channel channel)
         {
-            Channel.RemoveSubscriber(this);
-            Channel = null;
+            channel.RemoveSubscriber(this);
+            Subscriptions.Remove(channel);
         }
 
-        public void Update(string videoName)
+        public void Update(string videoName, string channelName, string url)
         {
-            Console.WriteLine($"[NOTIFICATION IN SUBSCRIBER CLASS] => Hey {Name} :: New Video Upload [{videoName}] from {Channel.channelName}");
+            Console.WriteLine($"[NOTIFICATION IN SUBSCRIBER CLASS] => Hey {Name} :: New Video Upload [{videoName}] from {channelName} on {url}");
         }
     }
 }
