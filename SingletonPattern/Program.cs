c6c20368e7ad6bba66fc7d57022bfdbe32e06813
MusiCollins <rafaelmilanes91@gmail.com>
2022-03-11 16:44:14 +0100

    Created Dev Branch

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -4,13 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            /*
+            
                 var conn = MysqlConnection_SINGLETON.getInstance();
                 ClassOneNeedsMysqlConnection_SINGLETON client1 = new ClassOneNeedsMysqlConnection_SINGLETON(conn);
                 ClassTwoNeedsMysqlConnection_SINGLETON client2 = new ClassTwoNeedsMysqlConnection_SINGLETON(conn);
                 client1.MakeDbCall();
                 client2.MakeDbCall();
-            */
+            
 
 
         }
